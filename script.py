d25c5906fad87acdbf1774bb62c8cb1624a35743
DavisWalkers <dimazverev716@gmail.com>
2020-04-02 16:23:37 +0300

    Added interest finder logic

--- script.py
+++ script.py
@@ -22,6 +22,7 @@ print(test_destination_index)
 attractions = [[] for dest in destinations]
 print(attractions)
 
+# Adding new attractions to the attractions list
 def add_attraction(destination, attraction):
     try:
         destination_index = get_destination_index(destination)
@@ -31,6 +32,7 @@ def add_attraction(destination, attraction):
     attractions_for_destination.append(attraction)
     return 
 
+# Adding some attractions
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
 add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
@@ -41,3 +43,17 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for possible_attraction in attractions_in_city:
+        attraction_tags = possible_attraction[1]
+        for interest in interests:
+            if (attraction_tags.count(interest) != 0):
+                attractions_with_interest.append(possible_attraction[0])
+    return attractions_with_interest
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
