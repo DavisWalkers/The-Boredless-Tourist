54594226528f731506fdf3ccc8443a63334a3c01
DavisWalkers <dimazverev716@gmail.com>
2020-04-02 15:43:18 +0300

    Added test objects

---
+++ script.py
@@ -0,0 +1,5 @@
+# Destinations list
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+# Test casa
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+print(destinations)
